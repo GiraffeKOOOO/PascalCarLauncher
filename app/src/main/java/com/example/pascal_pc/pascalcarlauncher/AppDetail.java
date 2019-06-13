bf112111be760b053992c8fcf2715c65841f3e2c
GiraffeKOOOO <pstepien@hotmail.co.uk>
2019-06-13 23:55:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pascal_pc/pascalcarlauncher/AppDetail.java
@@ -0,0 +1,11 @@
+package com.example.pascal_pc.pascalcarlauncher;
+
+import android.graphics.drawable.Drawable;
+
+public class AppDetail {
+
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+
+}
